329fc92a0530abacdfb483a6c042854d0446c04a
ElliotKnockIP <elliot.knock@installerpro.co.uk>
2025-05-07 16:42:06 +0100

    added in lines sort of working

--- scripts/image-upload.js
+++ scripts/image-upload.js
@@ -1,36 +1,51 @@
 export function initImageUpload(fabricCanvas, input) {
-  input.addEventListener('change', function () {
+  // Reference to the button
+  const chooseBackgroundBtn = document.getElementById("choose-background-btn");
+
+  // Add click event listener to the button to trigger file input
+  chooseBackgroundBtn.addEventListener("click", () => {
+    input.click();
+  });
+
+  input.addEventListener("change", function () {
     const file = this.files[0];
     if (!file) return;
 
     const url = URL.createObjectURL(file);
 
-    fabric.Image.fromURL(url, function (img) {
-      const canvasWidth = fabricCanvas.getWidth();
-      const canvasHeight = fabricCanvas.getHeight();
-      const imgWidth = img.width;
-      const imgHeight = img.height;
+    fabric.Image.fromURL(
+      url,
+      function (img) {
+        const canvasWidth = fabricCanvas.getWidth();
+        const canvasHeight = fabricCanvas.getHeight();
+        const imgWidth = img.width;
+        const imgHeight = img.height;
 
-      const scale = Math.min(canvasWidth / imgWidth, canvasHeight / imgHeight);
-      const left = (canvasWidth - imgWidth * scale) / 2;
-      const top = (canvasHeight - imgHeight * scale) / 2;
+        const scale = Math.min(
+          canvasWidth / imgWidth,
+          canvasHeight / imgHeight
+        );
+        const left = (canvasWidth - imgWidth * scale) / 2;
+        const top = (canvasHeight - imgHeight * scale) / 2;
 
-      img.set({
-        scaleX: scale,
-        scaleY: scale,
-        left: left,
-        top: top,
-        selectable: false
-      });
+        img.set({
+          scaleX: scale,
+          scaleY: scale,
+          left: left,
+          top: top,
+          selectable: false,
+        });
 
-      fabricCanvas.clear();
-      fabricCanvas.add(img);
-      fabricCanvas.sendToBack(img);
+        fabricCanvas.clear();
+        fabricCanvas.add(img);
+        fabricCanvas.sendToBack(img);
 
-      URL.revokeObjectURL(url);
-      input.value = "";
-    }, {
-      crossOrigin: 'anonymous'
-    });
+        URL.revokeObjectURL(url);
+        input.value = "";
+      },
+      {
+        crossOrigin: "anonymous",
+      }
+    );
   });
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
-import { initImageUpload } from './image-upload.js';
-import { initCanvasControls } from './canvas-controls.js';
-import { initDragDropIcons } from './drag-drop-icons.js'
+import { initImageUpload } from "./image-upload.js";
+import { initCanvasControls } from "./canvas-controls.js";
+import { initDragDropIcons } from "./drag-drop-icons.js";
+import { initAddWalls } from "./walls.js";
 
 window.onload = function () {
-  const fabricCanvas = new fabric.Canvas('layout-canvas');
-  const input = document.getElementById('choose-background');
-  const clearButton = document.getElementById('clear');
-  const downloadButton = document.getElementById('download');
+  const fabricCanvas = new fabric.Canvas("layout-canvas");
+  const input = document.getElementById("choose-background");
+  const clearButton = document.getElementById("clear");
+  const downloadButton = document.getElementById("download");
 
   initImageUpload(fabricCanvas, input);
   initCanvasControls(fabricCanvas, clearButton, downloadButton);
   initDragDropIcons(fabricCanvas);
+  initAddWalls(fabricCanvas);
 };
